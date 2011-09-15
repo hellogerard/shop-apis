9cf2d5a31a5681bf4cdcb6a1cfb396e0b94d7bee
gerard sychay <hellogerard@gmail.com>
2011-09-15 17:16:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Mashup of Google Search API for Shopping
+<<http://code.google.com/apis/shopping/search/>>, Google Suggest API
+<<http://blogoscoped.com/archive/2006-08-17-n22.html>>, and the ShopSavvy API
+<<http://shopsavvy.mobi/>>.
+
+Created for Cincy Hackathon <<http://cincyhackathon.com>>
+
+Currently deployed at <<http://shop-apis.pagodabox.com>>
+
+_see LICENSE for copyright and license info_
---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require(dirname(__DIR__) . '/lib/limonade.php');
+
+//
+// configuration
+//
+
+function configure()
+{
+    option('root_dir', dirname(__DIR__));
+    option('views_dir', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views');
+    option('controllers_dir', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'controllers');
+    option('lib_dir', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib');
+    option('public_dir', __DIR__);
+}
+
+function before()
+{
+    layout('layouts/layout.phtml');
+}
+
+//
+// application
+//
+
+dispatch('/', function() {
+    return html('index.phtml');
+});
+
+dispatch('/shopsavvy/:id', function($id) {
+
+    if (empty($id))
+    {
+        halt(NOT_FOUND);
+    }
+
+    $json = file_get_contents("http://api.developer.shopsavvy.mobi/products/{$id}.json?apikey=91f7ccdc3a32b5ba899e64fd191942c8");
+    return $json;
+});
+
+run();
